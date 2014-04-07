e08cc626b767272b0eb6187638775292f0ee1710
Dan Mayer <dan.mayer@livingsocial.com>
2014-04-07 13:10:57 -0400

    support differences in rails logger

--- Rakefile
+++ Rakefile
@@ -7,3 +7,12 @@ Rake::TestTask.new(:test) do |test|
   test.pattern = 'test/**/*_test.rb'
   test.verbose = true
 end
+
+
+# bundle exec rake console
+# LS::BrowseService.host = 'http://browse-service.browse-service.qa.livingsocial.net/'
+# LS::BrowseService::Client.new.get_category('washington-d-c','food-deals')
+desc "start a console with the gem loaded"
+task :console do
+  sh "irb -rubygems -I lib -r ls/browse-service-client.rb"
+end
--- lib/imprint/rails_logger.rb
+++ lib/imprint/rails_logger.rb
@@ -1,19 +1,33 @@
 if defined?(ActiveSupport::BufferedLogger)
-  #Rails 3
+  #Rails 2 and 3 support
   module ActiveSupport
     class BufferedLogger
       def add(severity, message = nil, progname = nil, &block)
-        return if @level > severity
-        message = (message || (block && block.call) || progname).to_s
-        # If a newline is necessary then create a new message ending with a newline.
-        # Ensures that the original message is not mutated.
-        message = "#{message}\n" unless message[-1] == "\n"
-        if (defined?(Imprint::Tracer)) && message && message.is_a?(String) && message.length > 1 && Imprint::Tracer.get_trace_id
-          message = message.gsub("\n"," [trace_id=#{Imprint::Tracer.get_trace_id}]\n")
+        #rails 2 and 3.0
+        if @level && self.respond_to?(:buffer)
+          return if !@level.nil? && (@level > severity)
+          message = (message || (block && block.call) || progname).to_s
+          # If a newline is necessary then create a new message ending with a newline.
+          # Ensures that the original message is not mutated.
+          message = "#{message}\n" unless message[-1] == "\n"
+          if (defined?(Imprint::Tracer)) && message && message.is_a?(String) && message.length > 1 && Imprint::Tracer.get_trace_id
+            message = message.gsub("\n"," [trace_id=#{Imprint::Tracer.get_trace_id}]\n")
+          end
+          buffer << message
+          auto_flush
+          message
+        else
+          # rails 3.2.x 
+          return if !level.nil? && (level > severity)
+          message = (message || (block && block.call) || progname).to_s
+          # If a newline is necessary then create a new message ending with a newline.
+          # Ensures that the original message is not mutated.
+          message = "#{message}\n" unless message[-1] == "\n"
+          if (defined?(Imprint::Tracer)) && message && message.is_a?(String) && message.length > 1 && Imprint::Tracer.get_trace_id
+            message = message.gsub("\n"," [trace_id=#{Imprint::Tracer.get_trace_id}]\n")
+          end
+          @log.add(severity, message, progname, &block)
         end
-        buffer << message
-        auto_flush
-        message
       end
     end
   end
